1a0b2832434b2ff3707882240036ae94ae5c3d84
venetak <veneta.kochovska93@gmail.com>
2025-05-02 21:33:30 +0300

    Add game menu screen

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module scourgehunt
 go 1.24.2
 
 require (
-	github.com/hajimehoshi/ebiten v1.12.12
 	github.com/hajimehoshi/ebiten/v2 v2.8.8
 	github.com/rendering v0.0.0-00010101000000-000000000000
 	github/actor v0.0.0-00010101000000-000000000000
@@ -14,13 +13,12 @@ require (
 	github.com/ebitengine/gomobile v0.0.0-20240911145611-4856209ac325 // indirect
 	github.com/ebitengine/hideconsole v1.0.0 // indirect
 	github.com/ebitengine/purego v0.8.0 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
+	github.com/go-text/typesetting v0.2.0 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
-	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.20.0 // indirect
-	golang.org/x/mobile v0.0.0-20210208171126-f462b3930c8f // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 )
 
 replace github/actor => ./actor
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	_ "image/png"
 	"log"
 
@@ -9,25 +10,54 @@ import (
 	"utils"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
 	"github.com/rendering"
 )
+
+type GameState string
+
+var (
+	arcadeFaceSource *text.GoTextFaceSource
+)
+
+func init() {
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(fonts.PressStart2P_ttf))
+	if err != nil {
+		log.Fatal(err)
+	}
+	arcadeFaceSource = s
+}
+
+// Constants defining the allowed game states
+const (
+	GameMenu    GameState = "Menu"
+	GameStarted GameState = "Started"
+	GamePaused  GameState = "Paused"
+	GameEnded   GameState = "Ended"
+)
+
+const sampleText = "Press space key to start"
 
 type Game struct {
 	scourgeActor *actor.Actor
 	purgerActor  *actor.Actor
 	NPCActors    []*actor.Actor
-	State        string
+	State        GameState
 	keys         []ebiten.Key
 }
 
 const (
-	screenWidth  = 1000
+	screenWidth  = 900
 	screenHeight = 400
+	lineSpacing  = 0
+	fontSize     = 10
 )
 
 func newGame(playerActor *actor.Actor, npcActors []*actor.Actor) *Game {
 	return &Game{
+		State:        GameMenu,
 		scourgeActor: playerActor,
 		purgerActor:  playerActor,
 		NPCActors:    npcActors,
@@ -36,35 +66,63 @@ func newGame(playerActor *actor.Actor, npcActors []*actor.Actor) *Game {
 
 func (g *Game) Update() error {
 	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
-	for npcActor := range g.NPCActors {
-		g.NPCActors[npcActor].Patrol()
-	}
-	g.NPCActors[0].Patrol()
-	if len(g.keys) > 0 {
-		g.purgerActor.HandleInput(g.keys)
-		// if g.purgerActor.CollidesWith(g.NPCActors[0]) {
-		// 	g.purgerActor.RollbackPosition()
-		// }
+
+	switch g.State {
+	case GameMenu:
+		if ebiten.IsKeyPressed(ebiten.KeySpace) {
+			g.State = GameStarted
+		}
+	case GamePaused:
+
+	case GameStarted:
+		for npcActor := range g.NPCActors {
+			g.NPCActors[npcActor].Patrol(10)
+		}
+		// g.NPCActors[0].Patrol(10)
+		if len(g.keys) > 0 {
+			g.purgerActor.HandleInput(g.keys)
+			if g.purgerActor.CollidesWith(g.NPCActors[0]) {
+				g.purgerActor.RollbackPosition()
+			}
+		}
 	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	// TODO: cache images?
-	for _, npc := range g.NPCActors {
-		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(npc.Position[0], npc.Position[1])
-		rendering.DrawImageWithMatrix(screen, npc.Image, op)
+	switch g.State {
+	case GameMenu:
+		op := &text.DrawOptions{}
+		op.LineSpacing = lineSpacing
+
+		width, height := text.Measure(sampleText, &text.GoTextFace{
+			Source: arcadeFaceSource,
+			Size:   fontSize,
+		}, lineSpacing)
+
+		op.GeoM.Reset()
+		op.GeoM.Translate(screenWidth/2-(width/2), height*5)
+		text.Draw(screen, sampleText, &text.GoTextFace{
+			Source: arcadeFaceSource,
+			Size:   fontSize,
+		}, op)
+	case GameStarted:
+		for _, npc := range g.NPCActors {
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(npc.Position[0], npc.Position[1])
+			rendering.DrawImageWithMatrix(screen, npc.Image, op)
+		}
+
+		opArthas := &ebiten.DrawImageOptions{}
+		opArthas.GeoM.Translate(g.purgerActor.Position[0], g.purgerActor.Position[1])
+
+		rendering.DrawImageWithMatrix(screen, g.purgerActor.Image, opArthas)
 	}
-
-	opArthas := &ebiten.DrawImageOptions{}
-	opArthas.GeoM.Translate(g.purgerActor.Position[0], g.purgerActor.Position[1])
-
-	rendering.DrawImageWithMatrix(screen, g.purgerActor.Image, opArthas)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 1000, 400
+	return 800, 400
 }
 
 func main() {
@@ -77,12 +135,12 @@ func main() {
 	ebiten.SetWindowTitle("Animation (Ebitengine Demo)")
 
 	playerActor := actor.NewActor([2]float64{0, 0}, purgerTexture, 14)
-	npcActor := actor.NewActor([2]float64{200, 200}, scourgeTexture, 0.5)
-	npcActor1 := actor.NewActor([2]float64{200, 200}, scourgeTexture1, 4)
+	npcActor := actor.NewActor([2]float64{200, 200}, scourgeTexture, 4)
+	npcActor1 := actor.NewActor([2]float64{300, 200}, scourgeTexture1, 1)
 	npcActor2 := actor.NewActor([2]float64{100, 200}, scourgeTexture1, 1)
-	npcActor3 := actor.NewActor([2]float64{150, 50}, scourgeTexture1, 1)
-	npcActor4 := actor.NewActor([2]float64{200, 200}, scourgeTexture1, 2)
-	npcActor5 := actor.NewActor([2]float64{200, 200}, scourgeTexture1, 0.5)
+	npcActor3 := actor.NewActor([2]float64{250, 50}, scourgeTexture1, 1)
+	npcActor4 := actor.NewActor([2]float64{300, 100}, scourgeTexture1, 1)
+	npcActor5 := actor.NewActor([2]float64{300, 300}, scourgeTexture1, 0.5)
 
 	if err := ebiten.RunGame(newGame(playerActor, []*actor.Actor{npcActor, npcActor1, npcActor2, npcActor3, npcActor4, npcActor5})); err != nil {
 		log.Fatal(err)
